18ac57cec052f590441825d7161cc748f0d8f176
RoHaNaLpHaCoDeR <rohanaroraaashi@gmail.com>
2022-05-16 10:30:55 +0530

    Java Programs

--- src/CWH_CH3_14_StringMethods.java
+++ src/CWH_CH3_14_StringMethods.java
@@ -19,9 +19,23 @@ public class CWH_CH3_14_StringMethods {
 //        System.out.println(firstName.substring(2));
 //        System.out.println(firstName.substring(0));
 //        System.out.println(firstName.substring(1,5));
-        System.out.println(firstName.replace("r","p"));
-        System.out.println(firstName.replace("an","pq"));
-        System.out.println(firstName.replace("r","pet"));
+//        System.out.println(firstName.replace("r","p"));
+//        System.out.println(firstName.replace("an","pq"));
+//        System.out.println(firstName.replace("r","pet"));
+//
+//        System.out.println(firstName.startsWith("Ro"));
+//        System.out.println(firstName.endsWith("rohan"));
+//
+//        System.out.println(firstName.charAt(2));
+
+        String modified_name = "Hello Rohan Arora";
+//        System.out.println(modified_name.indexOf("ro"));
+//        System.out.println(modified_name.indexOf("a", 4));
+        System.out.println(modified_name.lastIndexOf("h"));
+        System.out.println(modified_name.lastIndexOf("Roh",12));
+        System.out.println(firstName.equals("Rohan Arora"));
+        System.out.println(firstName.equalsIgnoreCase("rohan arora"));
+        System.out.println("I am \n escape \t sequence character \" " );
     }
 
 }
