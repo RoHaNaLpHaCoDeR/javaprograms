6d7ca20c9a48606de00929845b1f60e15aa8d865
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-05 21:10:54 +0530

    Java Programs

--- src/CWH_CH13_ExtendingRunnableInterface.java
+++ src/CWH_CH13_ExtendingRunnableInterface.java
@@ -1,5 +1,97 @@
+class MyRunnableThread1 implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+        System.out.println("Running Thread 1 ");
+    }
+}
+
+class MyRunnableThread2 implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+        System.out.println("Running Thread 2 ");
+    }
+}
+
 public class CWH_CH13_ExtendingRunnableInterface {
     public static void main(String[] args) {
+        MyRunnableThread1 bullet1 = new MyRunnableThread1();
+        MyRunnableThread2 bullet2 = new MyRunnableThread2();
+
+        Thread gun1 = new Thread(bullet1);
+        Thread gun2 = new Thread(bullet2);
+
+        gun1.start();
+        gun2.start();
 
     }
 }
