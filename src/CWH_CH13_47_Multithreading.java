2117faf6c28a1c66a88e2f21731e06b54d2e9da7
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-05 18:47:40 +0530

    Java Programs

---
+++ src/CWH_CH13_47_Multithreading.java
@@ -0,0 +1,18 @@
+public class CWH_CH13_47_Multithreading {
+    public static void main(String[] args) {
+//        func1(); executing along with func2
+//        func2(); executing along with func1
+        // Concurrency and Parallelism
+        /* Concurrency - perform multiple task at the same time
+        but doing only one task at single instant of time.
+
+        Parallelism - Performing multiple tasks at the same time
+        and executing all the tasks simultaneously.
+
+        If the OS is free the other task is executed which leads to
+        completing of multiple tasks at the same time leading to
+        less wastage of time and more of work so multithreading is
+        an important task in JAVA  programming.
+         */
+    }
+}
