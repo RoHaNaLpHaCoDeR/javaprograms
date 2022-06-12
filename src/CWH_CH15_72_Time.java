0a579636b8c8d61b7fbd8b2c532fa124026f2856
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-12 12:49:40 +0530

    Java Programs

--- src/CWH_CH15_72_Time.java
+++ src/CWH_CH15_72_Time.java
@@ -1,5 +1,14 @@
 public class CWH_CH15_72_Time {
     public static void main(String[] args) {
+        /*
+        java.time API - Classes & Methods
+        Date and time features in Java is primarily supported by two packages :
 
+        java.util
+        java.time
+        The package java.time was added with the release of Java 8 with the aim
+        of solving problems faced by Java developers while handling date and time
+        with java.util package such as representing a date without time, etc.
+         */
     }
 }
