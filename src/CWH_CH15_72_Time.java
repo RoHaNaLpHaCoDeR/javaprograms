f4611047d7fea3c4218de23b072262cddca23460
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-12 12:54:50 +0530

    Java Programs

--- src/CWH_CH15_72_Time.java
+++ src/CWH_CH15_72_Time.java
@@ -17,5 +17,31 @@ public class CWH_CH15_72_Time {
         Clock c2 = Clock.systemUTC();
         System.out.println(c2.instant());
 
+        Duration d1 = Duration.between(LocalTime.MIN,LocalTime.NOON);  //LocalTime.MIN = '00:00' , LocalTime.NOON = '12:00'
+        System.out.println(d1.isNegative());
+        System.out.println(d1.isZero());
+
+        Duration d2 = Duration.between(LocalTime.MAX,LocalTime.MIN);  //LocalTime.MAX =  '23:59:59.999999999' ,  LocalTime.MIN = '00:00'
+
+        System.out.println(d2.isNegative());
+        System.out.println(d2.isZero());
+
+        LocalDate d = LocalDate.now();
+        System.out.println(d);
+        LocalDate dl= LocalDate.parse("2021-05-26");
+        LocalDate dc= LocalDate.parse("2021-05-26");
+
+        System.out.println(dl.equals(d));
+        System.out.println(dc.equals(dl));
+
+        LocalTime t = LocalTime.now();
+        System.out.println("Time before : "  + t);
+
+        LocalTime t1= t.plusHours(5);
+        System.out.println("Time after adding 5 hours : " + t1);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+
     }
 }
