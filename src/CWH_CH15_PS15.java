5bb609bd81cce321e91e0f90959f7a5c6dd10135
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-14 17:38:48 +0530

    Java Programs

--- src/CWH_CH15_PS15.java
+++ src/CWH_CH15_PS15.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class CWH_CH15_PS15 {
     public static void main(String[] args) {
+        // PS Q1
+        ArrayList ar = new ArrayList();
+        ar.add("Student 1");
+        ar.add("Student 2");
+        ar.add("Student 3");
+        ar.add("Student 4");
+        ar.add("Student 5");
+        ar.add("Student 6");
+        ar.add("Student 7");
+        ar.add("Student 8");
+        ar.add("Student 9");
+        ar.add("Student 10");
+        for(Object o: ar){
+            System.out.println(o);
+        }
 
     }
 }
