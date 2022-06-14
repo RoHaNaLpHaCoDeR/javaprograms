e432061d6b2b7954e2d9fcc698e1f2bd763cba17
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-14 17:25:51 +0530

    Java Programs

--- src/CWH_CH15_73_DateTimeFormatter.java
+++ src/CWH_CH15_73_DateTimeFormatter.java
@@ -1,5 +1,17 @@
 public class CWH_CH15_73_DateTimeFormatter {
     public static void main(String[] args) {
-
+        /*
+        DateTimeFormatter in Java
+        This class helps us to print and parse date and time in our desired format.
+        The format() method of the DateTimeFormatter class is used to format the dates using our desired format.
+        Syntax :
+        public String format(DateTimeFormatter formatter)
+        Parameter :
+        The object of the formatter to be used is passed, and it can not be null.
+        Exception :
+        This method throws DateTimeException.
+        Return Value :
+        Returns the string in the format specified by the user.
+         */
     }
 }
