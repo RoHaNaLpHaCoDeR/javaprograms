71816f88c89a2db3a4ca595800c765da8f45162e
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-15 15:00:07 +0530

    Java Programs

--- src/CWH_CH16_74_LibraryManagement.java
+++ src/CWH_CH16_74_LibraryManagement.java
@@ -1,6 +1,9 @@
 public class CWH_CH16_74_LibraryManagement {
     public static void main(String[] args) {
         /*
+        Create a library management system which is capable of issuing books to the students.
+        Book should have info like:
+        1. Book name
 
          */
     }
