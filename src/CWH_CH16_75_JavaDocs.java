af2f436d70f7bfcdc6ef2a5673664a10c0d8c2a1
ROHAN ARORA <rohanaroraaashi@gmail.com>
2022-06-16 11:29:57 +0530

    Java Programs

---
+++ src/CWH_CH16_75_JavaDocs.java
@@ -0,0 +1,19 @@
+/**
+ * This class is to demonstrate what java doc is and how it is used in java industry.
+ * this is <i>italics </i> word <p>This is new paragraph </p>
+ * @author Rohan (CodewithRohan)
+ * @version 0.1
+ * @since 2018
+ * @see <a href="https://www.google.com/docs/about/" target = "_blank"></a>Google Docs
+ */
+
+public class CWH_CH16_75_JavaDocs {
+
+    public void add(int a ,int b){
+        System.out.println("This sum is : " + a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
+}
